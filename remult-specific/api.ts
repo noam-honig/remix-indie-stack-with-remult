bece770641cebdd3585c630710a261a546d1e4dd
Noam Honig <noam.honig@gmail.com>
2023-01-05 08:17:58 +0200

    some cleanup

--- remult-specific/api.ts
+++ remult-specific/api.ts
@@ -1,19 +1,20 @@
-import { createRemultServer } from 'remult/server';
-import { requireUserId } from '../app/session.server';
-import { Note } from '../app/models/note';
-import type { ActionArgs, SerializeFrom, TypedResponse } from "@remix-run/node";
-import { json } from "@remix-run/node";
+import { createRemultServer } from "remult/server";
+import { requireUserId } from "../app/session.server";
+import { Note } from "../app/models/note";
+import type { ActionArgs } from "@remix-run/node";
 
 export const api = createRemultServer<Request>({
   entities: [Note],
-  getUser: async request => ({ id: await requireUserId(request) })
+  getUser: async (request) => ({ id: await requireUserId(request) }),
 });
-export function withRemult<args extends ActionArgs, result>(what: (a: args) => result) {
-  return async (z: args) => {
-    return await new Promise<result>(async (res, reject) => {
-      api.withRemult(z.request, undefined!, async () => {
+export function withRemult<args extends ActionArgs, result>(
+  what: (args: args) => result
+) {
+  return (args: args) => {
+    return new Promise<result>(async (res, reject) => {
+      api.withRemult(args.request, undefined!, async () => {
         try {
-          res(await what(z));
+          res(await what(args));
         } catch (error: any) {
           reject(error);
         }
