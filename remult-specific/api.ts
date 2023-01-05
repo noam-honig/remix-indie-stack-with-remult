9856d97d43f8722dbbae06ba7097a664aa80eb78
Noam Honig <noam.honig@gmail.com>
2023-01-05 08:59:39 +0200

    enforced require user

--- remult-specific/api.ts
+++ remult-specific/api.ts
@@ -1,7 +1,8 @@
 import { createRemultServer } from "remult/server";
 import { getUserId } from "../app/session.server";
 import { Note } from "../app/models/note";
-import type { ActionArgs } from "@remix-run/node";
+import { ActionArgs, redirect } from "@remix-run/node";
+import { remult } from "remult";
 
 export const api = createRemultServer<Request>({
   entities: [Note],
@@ -18,6 +19,13 @@ export function withRemult<args extends ActionArgs, result>(
     new Promise<result>(async (resolve, reject) => {
       api.withRemult(args.request, undefined!, async () => {
         try {
+          if (!remult.authenticated()) {
+            const redirectTo = new URL(args.request.url).pathname;
+            const searchParams = new URLSearchParams([
+              ["redirectTo", redirectTo],
+            ]);
+            throw redirect(`/login?${searchParams}`);
+          }
           resolve(await what(args));
         } catch (error: any) {
           reject(error);
